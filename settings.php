131aa07af97ab62139b10dbcadc564398215fa5b
timurg <girfanov@outlook.com>
2014-09-19 12:25:22 +0600

    дополнение

---
+++ settings.php
@@ -0,0 +1,68 @@
+<?
+global $SETTINGS;
+$SETTINGS = array(
+    "KOMPAS_WS_URL"     => "http://portal.insto.ru:83/ws/kompas-asa.1cws?wsdl",
+	"ASA_WS_URL"     => "http://asa.insto.ru:3989/Service1.svc?wsdl",
+    "KOMPAS_WS_LOGIN"   => "",
+    "KOMPAS_WS_PASS"    => "",
+    "SUPERVISING_DEPARTMENTS"       => array(
+        1  => array(
+                    "head_first_name"   => "Татьяна",
+                    "head_last_name"    => "Гогузева",
+                    "head_patronymic"   => "Сергеевна"
+        ),
+        4  => array(
+                    "head_first_name"   => "Азат",
+                    "head_last_name"    => "Фаттахов",
+                    "head_patronymic"   => "Адифович"
+        ),
+        5  => array(
+                    "head_first_name"   => "Ильдус",
+                    "head_last_name"    => "Шаяхметов",
+                    "head_patronymic"   => "Фаатович"
+        ),
+        6  => array(
+                    "head_first_name"   => "Римма",
+                    "head_last_name"    => "Зинатуллина",
+                    "head_patronymic"   => "Раисовна"
+        ),
+        7  => array(
+                    "head_first_name"   => "Ирина",
+                    "head_last_name"    => "Боронилова",
+                    "head_patronymic"   => "Геннадьевна"
+        ),
+        8  => array(
+                    "head_first_name"   => "Наталья",
+                    "head_last_name"    => "Всеволодова",
+                    "head_patronymic"   => "Александровна"
+        ),
+        44  => array(
+                    "head_first_name"   => "Венер",
+                    "head_last_name"    => "Самигуллин",
+                    "head_patronymic"   => "Каллимулович"
+        ),
+        45  => array(
+                    "head_first_name"   => "Венер",
+                    "head_last_name"    => "Ситдиков",
+                    "head_patronymic"   => "Тимерханович"
+        ),
+        46  => array(
+                    "head_first_name"   => "Лев",
+                    "head_last_name"    => "Лившиц",
+                    "head_patronymic"   => "Владимирович"
+        ),
+        47  => array(
+                    "head_first_name"   => "Юрий",
+                    "head_last_name"    => "Шуматов",
+                    "head_patronymic"   => "Тимербаевич"
+        ),
+        48  => array(
+                    "head_first_name"   => "Наталья",
+                    "head_last_name"    => "Ахадуллина",
+                    "head_patronymic"   => "Эдуардовна"
+        )
+    )
+    
+);
+
+?>
